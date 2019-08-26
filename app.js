b70329ae5226036528a1cd9861ec5f7ecec959ed
Rahul Shaw <rshaw@q3infotech.net>
2019-08-26 18:12:37 +0530

    Added views tamplate, mongo db connection ,forms validation and requests

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 const app = express();
 
-//EJS
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-//Routes
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }))
+
+// Routes
 app.use('/', require('./routes'));
 app.use('/users', require('./routes/users'));
 const PORT = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://shaw:<password>@node-passport-auth-d6aie.mongodb.net/test?retryWrites=true&w=majority'
+}
